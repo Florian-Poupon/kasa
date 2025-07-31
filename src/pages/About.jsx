cafa9dc9f89436aa787e0be818dfcaaca3dc14dc
Florian Poupon <pro.poupon.fl@gmail.com>
2025-07-31 15:42:49 +0200

    Initialisation du projet Kasa avec React, Vite, React Router et Sass

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>À propos</h1>
+      <p>Cette application a été créée avec :</p>
+      <ul>
+        <li>React</li>
+        <li>Vite</li>
+        <li>React Router</li>
+        <li>Sass</li>
+      </ul>
+    </div>
+  );
+}
+
+export default About; 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenue sur Kasa</h1>
+      <p>Votre application React avec Vite, React Router et Sass</p>
+    </div>
+  );
+}
+
+export default Home; 
