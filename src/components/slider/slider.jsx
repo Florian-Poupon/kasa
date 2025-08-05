fb879a05f78c46dcded32a330daaf03286cb1fbe
Florian Poupon <pro.poupon.fl@gmail.com>
2025-08-05 13:27:51 +0200

    Feat : slider logement

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import React, { useState } from "react";
+import Logements from "../../data/logements.json";
+
+export default function Slider() {
+  const { id } = useParams();
+  const logement = Logements.find((item) => item.id === id);
+
+  if (!logement) {
+    return <Error />;
+  }
+
+  const [index, setIndex] = useState(0);
+  const pictures = logement.pictures;
+
+  const prevSlide = () => setIndex(index === 0 ? pictures.length - 1 : index - 1);
+  const nextSlide = () => setIndex(index === pictures.length - 1 ? 0 : index + 1);
+
+  return (
+    <div className="slider">
+      <div className="slider__buttons">
+        <button onClick={prevSlide}>
+          <img src="/src/assets/images/icons/arrow_left.svg" alt="arrow left" />
+        </button>
+        <button onClick={nextSlide}>
+          <img src="/src/assets/images/icons/arrow_right.svg" alt="arrow right" />
+        </button>
+      </div>
+      <div className="slider__counter">
+        {index + 1} / {pictures.length}
+      </div>
+      <img src={pictures[index]} alt={logement.title} className="slider__image" />
+    </div>
+  );
+}
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Logements from "../data/logements.json";
 import Error from "./Error";
+import Slider from "../components/slider/Slider";
 
 export default function Logement() {
   const { id } = useParams();
   const logement = Logements.find((item) => item.id === id);
 
   if (!logement) {
-    // Afficher la page d'erreur si le logement n'existe pas
     return <Error />;
   }
 
   return (
     <>
-      <h1>{logement.title}</h1>
-      <p>{logement.description}</p>
-      {/* Ajoute ici les autres infos (images, etc.) */}
+      <Slider />
+      {/* <h1>{logement.title}</h1>
+      <p>{logement.description}</p> */}
     </>
   );
 }
